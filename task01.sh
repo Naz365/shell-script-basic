f545d4651fbb112992f67f478d696ba70dd04dca
Mohammad Nazmul Hossain <6874870-Naz365@users.noreply.gitlab.com>
2021-11-11 21:54:05 +0600

    task commit

--- task01.sh
+++ task01.sh
@@ -1,21 +1,15 @@
 #! /bin/bash
 
-myArr=( 17 7 5 9 2 )
+myArr=(17 7 5 9 2)
 
-for i in "${myArr[@]}"
-do
+# for i in "${myArr[@]}"
+for i in "${myArr[@]}"; do
 
+    if [ $(expr $i % 2) == 0 ]; then
+        echo "$i is even"
+    else
 
+        echo "$i is Odd value"
 
-if [ `expr $i % 2` == 0 ]
-
-then
-	echo "$i is even"
-else
-
-
-    echo "$i is Odd value"
-    
-fi
+    fi
 done
-
